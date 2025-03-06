6371d526684ece26a3a5cc9ccc982800d2ce6553
Mina <mina.hany@hotmail.com>
2025-03-05 19:55:57 -0500

    following code conventions

--- SEON_6441/src/main/java/com/Game/Territory.java
+++ SEON_6441/src/main/java/com/Game/Territory.java
@@ -8,14 +8,14 @@ import java.util.List;
  * and can have neighbors (other territories). It also keeps track of its owner, reserved armies, and whether it has been visited.
  */
 public class Territory {
-    private String l_name;
-    private String l_continent;
-    private int l_bonus;
-    private int l_numOfArmies;
-    private Player l_owner;
-    private int l_numOfReservedArmies;
-    private boolean l_isVisited;
-    private List<Territory> l_neighborList;
+    private String d_name;
+    private String d_continent;
+    private int d_bonus;
+    private int d_numOfArmies;
+    private Player d_owner;
+    private int d_numOfReservedArmies;
+    private boolean d_isVisited;
+    private List<Territory> d_neighborList;
 
     /**
      * Constructor to initialize a territory with a name, continent, and bonus value.
@@ -25,14 +25,14 @@ public class Territory {
      * @param bonus The bonus value of the territory.
      */
     public Territory(String name, String continent, int bonus) {
-        this.l_name = name;
-        this.l_continent = continent;
-        this.l_bonus = bonus;
-        this.l_numOfArmies = 0;
-        this.l_owner = null;
-        this.l_numOfReservedArmies = 0;
-        this.l_isVisited = false;
-        this.l_neighborList = new ArrayList<>();
+        this.d_name = name;
+        this.d_continent = continent;
+        this.d_bonus = bonus;
+        this.d_numOfArmies = 0;
+        this.d_owner = null;
+        this.d_numOfReservedArmies = 0;
+        this.d_isVisited = false;
+        this.d_neighborList = new ArrayList<>();
     }
 
     /**
@@ -41,13 +41,13 @@ public class Territory {
      * @param p_territory The Territory to copy.
      */
     public Territory(Territory p_territory) {
-        this(p_territory.l_name, p_territory.l_continent, p_territory.l_bonus);
-        this.l_numOfArmies = p_territory.l_numOfArmies;
-        this.l_numOfReservedArmies = p_territory.l_numOfReservedArmies;
-        this.l_owner = p_territory.l_owner;
+        this(p_territory.d_name, p_territory.d_continent, p_territory.d_bonus);
+        this.d_numOfArmies = p_territory.d_numOfArmies;
+        this.d_numOfReservedArmies = p_territory.d_numOfReservedArmies;
+        this.d_owner = p_territory.d_owner;
 
-        for (Territory neighbor : p_territory.l_neighborList) {
-            this.l_neighborList.add(neighbor);
+        for (Territory neighbor : p_territory.d_neighborList) {
+            this.d_neighborList.add(neighbor);
         }
     }
 
@@ -56,28 +56,28 @@ public class Territory {
      *
      * @return The name of the territory.
      */
-    public String getName() { return l_name; }
+    public String getName() { return d_name; }
 
     /**
      * Gets the continent that the territory belongs to.
      *
      * @return The continent of the territory.
      */
-    public String getContinent() { return l_continent; }
+    public String getContinent() { return d_continent; }
 
     /**
      * Gets the bonus value of the territory.
      *
      * @return The bonus value of the territory.
      */
-    public int getBonus() { return l_bonus; }
+    public int getBonus() { return d_bonus; }
 
     /**
      * Gets the number of armies in the territory.
      *
      * @return The number of armies in the territory.
      */
-    public int getNumOfArmies() { return l_numOfArmies; }
+    public int getNumOfArmies() { return d_numOfArmies; }
 
     /**
      * Sets the number of armies in the territory.
@@ -85,28 +85,28 @@ public class Territory {
      * @param p_numOfArmies The number of armies to set.
      */
     public void setNumOfArmies(int p_numOfArmies) {
-        this.l_numOfArmies = p_numOfArmies; }
+        this.d_numOfArmies = p_numOfArmies; }
 
     /**
      * Gets the owner of the territory.
      *
      * @return The player who owns the territory.
      */
-    public Player getOwner() { return l_owner; }
+    public Player getOwner() { return d_owner; }
 
     /**
      * Sets the owner of the territory.
      *
      * @param p_owner The player to set as the owner.
      */
-    public void setOwner(Player p_owner) { this.l_owner = p_owner; }
+    public void setOwner(Player p_owner) { this.d_owner = p_owner; }
 
     /**
      * Gets the number of reserved armies in the territory.
      *
      * @return The number of reserved armies in the territory.
      */
-    public int getNumOfReservedArmies() { return l_numOfReservedArmies; }
+    public int getNumOfReservedArmies() { return d_numOfReservedArmies; }
 
     /**
      * Sets the number of reserved armies in the territory.
@@ -114,14 +114,14 @@ public class Territory {
      * @param p_numOfReservedArmies The number of reserved armies to set.
      */
     public void setNumOfReservedArmies(int p_numOfReservedArmies) {
-        this.l_numOfReservedArmies = p_numOfReservedArmies; }
+        this.d_numOfReservedArmies = p_numOfReservedArmies; }
 
     /**
      * Gets the list of neighboring territories.
      *
      * @return A list of neighboring territories.
      */
-    public List<Territory> getNeighborList() { return l_neighborList; }
+    public List<Territory> getNeighborList() { return d_neighborList; }
 
     /**
      * Checks if a given territory is a neighbor of this territory.
@@ -131,7 +131,7 @@ public class Territory {
      */
     public boolean hasNeighbor(Territory p_target) {
 
-        return l_neighborList.contains(p_target);
+        return d_neighborList.contains(p_target);
     }
 
     /**
@@ -141,7 +141,7 @@ public class Territory {
      */
     public void addNeighbor(Territory p_neighbor) {
 
-        l_neighborList.add(p_neighbor);
+        d_neighborList.add(p_neighbor);
     }
 
     /**
@@ -151,8 +151,8 @@ public class Territory {
      */
     public List<Territory> getEnemyNeighbors() {
         List<Territory> enemyNeighbors = new ArrayList<>();
-        for (Territory neighbor : l_neighborList) {
-            if (neighbor.getOwner() != this.l_owner) {
+        for (Territory neighbor : d_neighborList) {
+            if (neighbor.getOwner() != this.d_owner) {
                 enemyNeighbors.add(neighbor);
             }
         }
@@ -171,7 +171,7 @@ public class Territory {
             return false;
         }
         Territory t = (Territory)p_obj;
-        return t.getName() == this.l_name && t.getContinent() == this.l_continent;
+        return t.getName() == this.d_name && t.getContinent() == this.d_continent;
     }
 
     /**
@@ -181,14 +181,14 @@ public class Territory {
      */
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Territory Name: ").append(l_name)
-                .append(" Continent: ").append(l_continent)
-                .append(" Number of Armies: ").append(l_numOfArmies)
+        StringBuilder l_sb = new StringBuilder();
+        l_sb.append("Territory Name: ").append(d_name)
+                .append(" Continent: ").append(d_continent)
+                .append(" Number of Armies: ").append(d_numOfArmies)
                 .append(" Neighbors: ");
-        for (Territory neighbor : l_neighborList) {
-            sb.append(neighbor.getName()).append(", ");
+        for (Territory neighbor : d_neighborList) {
+            l_sb.append(neighbor.getName()).append(", ");
         }
-        return sb.toString();
+        return l_sb.toString();
     }
 }
